5b722367521b4a8b2c4052da6e9e12546e11ae09
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-12 06:02:34 +0800

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -2,8 +2,8 @@ export default {
 	title: `Echozone | AUTOMATED RADIOSTREAMING PLATFORM 
 WITH CRYPTOCURRENCY EARNING FEATURE `,
 	description: `A marketing website theme built with ECHOZONE DEVELOPMENT DEPARTMENT and carefully crafted for startups and businesses..`,
-	url: `http://echozoneph.online`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
+	url: `https://echozonekanegosyoapp.netlify.app`, // No trailing slash!
+	name: `Echozone`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
