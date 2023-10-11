b42b37e74f1ba1108adffa4e7cbbde234122fe98
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-12 06:50:17 +0800

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -4,6 +4,6 @@ WITH CRYPTOCURRENCY EARNING FEATURE `,
 	description: `A marketing website theme built with ECHOZONE DEVELOPMENT DEPARTMENT and carefully crafted for startups and businesses..`,
 	url: `https://echozonekanegosyoapp.netlify.app`, // No trailing slash!
 	name: `Echozone`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: false,
+  enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
