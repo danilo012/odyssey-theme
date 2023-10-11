364fa999665216a20f1b024d06bd310ce62cc59b
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-11 19:09:22 +0800

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	title: `Echozone | AUTOMATED RADIOSTREAMING PLATFORM 
+WITH CRYPTOCURRENCY EARNING FEATURE `,
+	description: `A marketing website theme built with ECHOZONE DEVELOPMENT DEPARTMENT and carefully crafted for startups and businesses..`,
+	url: `http://echozoneph.online`, // No trailing slash!
 	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
